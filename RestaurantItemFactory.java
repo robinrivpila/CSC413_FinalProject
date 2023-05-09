8c19254d911849d6d5d3ab410ce200b1454df7b9
Robin Pilapil <robin.pilapil@gmail.com>
2023-05-09 16:00:36 -0700

    adding factory bev functionality

--- RestaurantItemFactory.java
+++ RestaurantItemFactory.java
@@ -15,6 +15,13 @@ public class RestaurantItemFactory {
                 Food newFood = new Food(foodItem, notes);
                 return newFood;
             case "beverage" :
+                System.out.println("Which item would you like to add to cart? ");
+                String bevItem = scan.nextLine();
+                System.out.println("Sweetness level? (0-100)");
+                int sweetnessLvl = scan.nextInt();
+                System.out.println("Ice level? ()");
+                //WOW HOW SHOULD I PARSE THIS
+
                 break;
             case "dessert" :
                 break;
